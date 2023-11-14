94c3c97f72f2e48748f6d363ef8dc9f57524442c
eoneee <87771474+eoneee@users.noreply.github.com>
2023-11-14 11:45:21 +0900

    [Silver V] Title: 영화감독 숌, Time: 52 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int cnt;
+
+// bool isDevilNum(int num){
+// 	string temp = to_string(num);
+// 	for(int i = 1; i < temp.length() - 1 ; i++){
+// 		if(temp[i] == temp [i-1] && temp[i] == temp[i+1] && temp[i] == '6'){
+// 			return true;
+// 		}
+// 	}
+// 	return false;
+// }
+bool isDevilNum(int num){
+	string temp = to_string(num);
+	for(int i = 0; i < int(temp.length()) - 2 ; i++){
+		if(temp[i] == temp [i+1] && temp[i] == temp[i+2] && temp[i] == '6'){
+			return true;
+		}
+	}
+	return false;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	for(int i = 1; ;i++){
+		if(isDevilNum(i)){
+			cnt++;
+			if(cnt == n){
+				cout << i;
+				return 0;
+			}
+		}
+	}
+	return 0;
+}
