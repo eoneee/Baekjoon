4e98f47677b7ecd0be33a097ff1acf3d66955bad
eoneee <87771474+eoneee@users.noreply.github.com>
2023-11-14 11:46:11 +0900

    [Silver V] Title: 영화감독 숌, Time: 52 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -2,24 +2,24 @@
 using namespace std;
 int cnt;
 
-// bool isDevilNum(int num){
-// 	string temp = to_string(num);
-// 	for(int i = 1; i < temp.length() - 1 ; i++){
-// 		if(temp[i] == temp [i-1] && temp[i] == temp[i+1] && temp[i] == '6'){
-// 			return true;
-// 		}
-// 	}
-// 	return false;
-// }
-bool isDevilNum(int num){
-	string temp = to_string(num);
-	for(int i = 0; i < int(temp.length()) - 2 ; i++){
-		if(temp[i] == temp [i+1] && temp[i] == temp[i+2] && temp[i] == '6'){
-			return true;
-		}
-	}
-	return false;
-}
+ bool isDevilNum(int num){
+ 	string temp = to_string(num);
+ 	for(int i = 1; i < temp.length() - 1 ; i++){
+ 		if(temp[i] == temp [i-1] && temp[i] == temp[i+1] && temp[i] == '6'){
+ 			return true;
+ 		}
+ 	}
+ 	return false;
+ }
+//bool isDevilNum(int num){
+//	string temp = to_string(num);
+//	for(int i = 0; i < int(temp.length()) - 2 ; i++){
+//		if(temp[i] == temp [i+1] && temp[i] == temp[i+2] && temp[i] == '6'){
+//			return true;
+//		}
+//	}
+//	return false;
+//}
 
 int main() {
 	int n;
