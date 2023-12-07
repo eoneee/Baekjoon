0523e2ebb4b077c629d23948f6ca13ee28b7ea85
eoneee <87771474+eoneee@users.noreply.github.com>
2023-12-07 14:28:52 +0900

    [Silver II] Title: 참외밭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2477. 참외밭/참외밭.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+pair<int, int> ground[12];
+
+int main() {
+	int n;
+	int dir, len;
+	int bigArea = 0;
+	int smallArea = 0;
+	cin >> n;
+	
+	for(int i = 0; i < 6; i++){
+		cin >> dir >> len;
+		ground[i] = ground[i + 6] = {dir, len};
+	}
+	
+	for(int i = 3; i < 12; i++){
+		if(ground[i].first == ground[i - 2].first && ground[i - 1].first == ground[i - 3].first){
+			bigArea = ground[i + 1].second * ground[i + 2].second;
+			smallArea = ground[i - 1].second * ground[i - 2].second;
+			break;
+		}
+	}
+	
+	cout << n * (bigArea - smallArea) << endl;
+	
+	return 0;
+}
