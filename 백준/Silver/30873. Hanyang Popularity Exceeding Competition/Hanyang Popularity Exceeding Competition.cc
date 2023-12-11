67529220b46b8f52530cbe65af5d60c9fd7790ed
eoneee <87771474+eoneee@users.noreply.github.com>
2023-12-12 00:29:12 +0900

    [Silver V] Title: Hanyang Popularity Exceeding Competition, Time: 108 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/30873. Hanyang Popularity Exceeding Competition/Hanyang Popularity Exceeding Competition.cc
+++ 백준/Silver/30873. Hanyang Popularity Exceeding Competition/Hanyang Popularity Exceeding Competition.cc
@@ -11,16 +11,11 @@ int main() {
 				cin >> celebrities[i].first >> celebrities[i].second;
 		}
 		int current = 0;
-		int maxx = 0;
 		for (int i = 0; i < N; ++i) {
 				int popularitySub = abs(celebrities[i].first - current);
 				if (popularitySub <= celebrities[i].second) {
-						// current += popularitySub;
 						current++;
-						// maxx = max(maxx, current);
-						// current++;
 				}
-			// cout << current << endl;
 		}
 
 		cout << current << endl;
