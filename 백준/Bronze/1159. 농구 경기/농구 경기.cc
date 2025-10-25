3cc9a85ebc71ae35a120bafb1dd5e933b0fae2c1
eoneee <87771474+eoneee@users.noreply.github.com>
2025-10-25 20:42:16 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.cc
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -1,36 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<string> name;
-
+int n, cnt[26];
+string s, result;
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	vector<char> ans;
-	int n;
 	cin >> n;
-	string temp;
 	for(int i = 0; i < n; i++){
-		cin >> temp;
-		name.push_back(temp);
+		cin >> s;
+		cnt[s[0] - 'a']++;
 	}
-	for(int i = 0; i < n; i++){
-	int cnt = 0;
-		for(int j = i + 1; j < n; j++){
-			if(name[i][0] == name[j][0]){
-				cnt ++;
-				if(cnt >= 4){
-					ans.push_back(name[i][0]);
-				}
-				//cout << name[i];			
-			}
+	for(int i = 0; i < 26; i++){
+		if(cnt[i] >= 5){
+			result += (i + 'a');
 		}
 	}
-	sort(ans.begin(), ans.end());
-	ans.erase(unique(ans.begin(), ans.end()), ans.end());
-	if(ans.empty()) cout << "PREDAJA";
-	for(int i = 0; i < ans.size(); i++){
-		cout << ans[i];
-	}
+	if(result.size()) cout << result << "\n";
+	else cout << "PREDAJA" << "\n";
 	return 0;
 }
